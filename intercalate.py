10b28108cf64203b145815bff79c2e0b02f7060a
Ashok Menon <amenon94@gmail.com>
2022-07-16 19:25:40 +0100

    Argument Parsing

--- intercalate.py
+++ intercalate.py
@@ -1,1 +1,36 @@
-print("Hello, world")
+import argparse
+
+parser = argparse.ArgumentParser(
+    description="Merges scans of front and back pages into one document"
+)
+
+parser.add_argument(
+    '--front', '-F',
+    type=str,
+    required=True,
+    help='PDF containing front pages',
+)
+
+parser.add_argument(
+    '--back', '-B',
+    type=str,
+    required=True,
+    help='PDF containing back pages (must contain same number of pages as FRONT)',
+)
+
+parser.add_argument(
+    '--out', '-O',
+    type=str,
+    required=True,
+    help='PDF to output to',
+)
+
+parser.add_argument(
+    '--reverse', '-R',
+    action='store_true',
+    help="Whether pages in BACK are reversed relative to FRONT.",
+)
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    print("Hello, world")
